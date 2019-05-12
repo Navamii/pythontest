10b1237ca8b3053cecfa6849bb935afb9cf3f4ad
Navamii <navami.94@gmail.com>
2019-05-12 19:01:28 +0530

    new change commit

--- Even_odd.py
+++ Even_odd.py
@@ -1,20 +1,20 @@
 #Program to check if a number is even or odd.
 
+while True:
+ try:
+    num = int(input("Enter a number: "))
 
-
-
-
-num = int(input("Enter a number: "))
-
-if num < 0 :
-
-	print("Please enter positive integer only")
+ except ValueError:
+    print("Oops!  That was no valid number.  Try again...")
+			
+ if num < 0 :
+    print("Please enter positive integer only")
 	
-	exit()
-
-
+ if (num % 2) == 0 :
+   print("{0} number is even".format(num))
+ else :
+   print("{0} number is odd".format(num))
  
-if (num % 2) == 0 :
-		print("{0} number is even".format(num))
-else :
-		print("{0} number is odd".format(num))
+
+
+
