c83fa36a20cf52f800ce532ead6e3b3aa8412800
Navamii <navami.94@gmail.com>
2019-05-12 19:18:34 +0530

    new change commit 2

--- Even_odd.py
+++ Even_odd.py
@@ -2,18 +2,19 @@
 
 while True:
  try:
-    num = int(input("Enter a number: "))
+  num = int(input("Enter a number: "))
+  if num % 2 == 0 :
+   print("{0} number is even".format(num))
+  else :
+   print("{0} number is odd".format(num))
 
  except ValueError:
     print("Oops!  That was no valid number.  Try again...")
-			
+ 		
  if num < 0 :
     print("Please enter positive integer only")
 	
- if (num % 2) == 0 :
-   print("{0} number is even".format(num))
- else :
-   print("{0} number is odd".format(num))
+
  
 
 
